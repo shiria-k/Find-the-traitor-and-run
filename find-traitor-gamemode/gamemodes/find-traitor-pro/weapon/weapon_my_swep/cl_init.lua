c06065993d4aa64e846e33a7c29c8af9755fa3d5
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-26 21:09:35 +0100

    Update cl_init.lua

--- find-traitor-gamemode/gamemodes/find-traitor-pro/weapon/weapon_my_swep/cl_init.lua
+++ find-traitor-gamemode/gamemodes/find-traitor-pro/weapon/weapon_my_swep/cl_init.lua
@@ -1,1 +1,26 @@
+include("shared.lua") 
 
+SWEP.DrawCrosshair = true
+
+SWEP.Slot = 1                
+SWEP.SlotPos = 1             
+SWEP.DrawAmmo = true         
+SWEP.BounceWeaponIcon = true
+
+local clr = Color(255, 50, 50, 255)
+function SWEP:DrawHUD()
+    local x, y = ScrW() / 2, ScrH() / 2
+    surface.SetDrawColor(clr)
+    surface.DrawRect(x - 2, y - 2, 4, 4) 
+end
+
+function SWEP:TranslateFOV(current_fov)
+    return current_fov 
+end
+
+function SWEP:PrintWeaponInfo(x, y, alpha)
+    draw.SimpleText("SCHADEN: HOCH", "DermaDefault", x, y, Color(255, 0, 0, alpha))
+    draw.SimpleText("NUR FÜR TRAITOR", "DermaDefault", x, y + 15, Color(255, 255, 255, alpha))
+end
+
+print("[FTP] Client-Teil der Waffe geladen!")
