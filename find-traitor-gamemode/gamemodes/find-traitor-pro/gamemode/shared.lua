9aee7b38e3ba25b242ec5dfb3ac486b4d3f59b58
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-26 18:02:17 +0100

    Update shared.lua

--- find-traitor-gamemode/gamemodes/find-traitor-pro/gamemode/shared.lua
+++ find-traitor-gamemode/gamemodes/find-traitor-pro/gamemode/shared.lua
@@ -1,1 +1,10 @@
+GM.Name = "Find Traitor Pro"
+GM.Author = "shirasophie"
 
+-- Rollen Definition
+ROLE_INNOCENT = 1
+ROLE_TRAITOR = 2
+
+function GM:Initialize()
+    self.BaseClass.Initialize(self)
+end
