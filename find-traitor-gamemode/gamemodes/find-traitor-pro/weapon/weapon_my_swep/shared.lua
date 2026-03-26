af56aa01a596f97be3da4f89c1a34a571cfb9b6b
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-26 21:18:12 +0100

    Update shared.lua

--- find-traitor-gamemode/gamemodes/find-traitor-pro/weapon/weapon_my_swep/shared.lua
+++ find-traitor-gamemode/gamemodes/find-traitor-pro/weapon/weapon_my_swep/shared.lua
@@ -1,1 +1,45 @@
+SWEP.PrintName = "Traitor Pistol"
+SWEP.Author = "Shira"
+SWEP.Instructions = "Starke Waffe für Verräter"
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
 
+SWEP.ViewModel = "models/weapons/c_pistol.mdl"
+SWEP.WorldModel = "models/weapons/w_pistol.mdl"
+SWEP.UseHands = true
+
+SWEP.Primary.ClipSize = 10
+SWEP.Primary.DefaultClip = 20
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = "Pistol"
+
+
+SWEP.PrintName    = "Traitor Pistol"        
+SWEP.Author       = "shira"
+SWEP.Instructions = "Linksklick: Schießen | Rechtsklick: Zoom"
+SWEP.Spawnable    = true                    
+SWEP.AdminOnly    = false                  
+
+SWEP.ViewModel    = "models/weapons/c_pistol.mdl"
+SWEP.WorldModel   = "models/weapons/w_pistol.mdl"
+SWEP.UseHands     = true                    
+
+-- Primär-Feuer (Linksklick)
+SWEP.Primary.ClipSize    = 10               
+SWEP.Primary.DefaultClip = 30               
+SWEP.Primary.Automatic   = true            
+SWEP.Primary.Ammo        = "Pistol"         
+SWEP.Primary.Delay       = 0.15             
+
+-- Sekundär-Feuer (Rechtsklick)
+SWEP.Secondary.ClipSize    = -1             
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic   = false
+SWEP.Secondary.Ammo        = "none"
+
+SWEP.Slot    = 1                            
+SWEP.SlotPos = 1                            
+
+function SWEP:Initialize()
+    self:SetHoldType("pistol")              
+end
