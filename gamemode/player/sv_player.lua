a4ce433d87f9fbcc91758319736b3a90b028e639
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-27 09:50:48 +0100

    Update sv_player.lua

--- gamemode/player/sv_player.lua
+++ gamemode/player/sv_player.lua
@@ -8,3 +8,9 @@ end)
 hook.Add("PlayerSpawn", "FTTR_Respawn", function(ply)
     ply:StripWeapons()
 end)
+hook.Add("PlayerInitialSpawn", "FTTR_Init", function(ply)
+    ply:SetModel("models/player/tm_leet_variantA.mdl") 
+    ply.FTTR_Money = FTTR.Config.StartMoney
+    ply.FTTR_Wins = 0
+    ply.FTTR_Role = FTTR.Roles.INNOCENT
+end)
