f9f2e0470f840aad09c2253bcbb169425348867f
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-27 09:39:30 +0100

    Update sv_safezone.lua

--- gamemode/system/sv_safezone.lua
+++ gamemode/system/sv_safezone.lua
@@ -1,1 +1,7 @@
+hook.Add("EntityTakeDamage", "FTTR_Safezone", function(target, dmg)
+    if not target:IsPlayer() then return end
 
+    if target:GetPos():Distance(FTTR.SpawnPos) < FTTR.Config.SpawnProtectionRadius then
+        return true
+    end
+end)
