ca6688a6aeed79fcc980fe008dec21283be74bd9
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-27 13:41:40 +0100

    Update cl_hud.lua

--- gamemodes/find-traitor-pro/gamemode/cl_hud.lua
+++ gamemodes/find-traitor-pro/gamemode/cl_hud.lua
@@ -1,1 +1,9 @@
+hook.Add("HUDPaint", "FTP_HUD", function()
+    local ply = LocalPlayer()
 
+    draw.SimpleText("Rolle: " .. ply:GetNWString("Role"),
+        "Trebuchet24", ScrW()-200, 20, Color(255,255,255))
+
+    draw.SimpleText("Geld: $" .. ply:GetNWInt("Money"),
+        "Trebuchet24", ScrW()-200, 50, Color(0,255,0))
+end)
