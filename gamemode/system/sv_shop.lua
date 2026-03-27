cba4a020412b159c9d3c5c52c2e8db47d9981a47
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-27 09:46:55 +0100

    Update sv_shop.lua

--- gamemode/system/sv_shop.lua
+++ gamemode/system/sv_shop.lua
@@ -1,1 +1,12 @@
-
+hook.Add("PlayerSay", "FTTR_Shop", function(ply, text)
+    if text == "!shop" then
+        if ply.FTTR_Money >= 100 then
+            ply:Give("weapon_shotgun")
+            ply.FTTR_Money = ply.FTTR_Money - 100
+            ply:ChatPrint("Shotgun gekauft!")
+        else
+            ply:ChatPrint("Nicht genug Geld!")
+        end
+        return ""
+    end
+end)
