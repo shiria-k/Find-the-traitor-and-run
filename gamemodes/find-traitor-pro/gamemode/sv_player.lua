7359dc208b77d46dc7d5bb701511f5b9954c1b01
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-27 13:40:53 +0100

    Update sv_player.lua

--- gamemodes/find-traitor-pro/gamemode/sv_player.lua
+++ gamemodes/find-traitor-pro/gamemode/sv_player.lua
@@ -1,1 +1,34 @@
+hook.Add("PlayerInitialSpawn", "FTP_Init", function(ply)
+    FTP:ResetPlayer(ply)
+    ply:SetModel("models/player/Group01/tm_leet_variantA.mdl")
+end)
 
+hook.Add("PlayerSpawn", "FTP_Spawn", function(ply)
+    ply:SetNWInt("Wins", ply:GetNWInt("Wins", 0))
+end)
+
+hook.Add("EntityTakeDamage", "NoSpawnDamage", function(target, dmg)
+    if target:IsPlayer() then
+        if target:GetPos():Distance(FTP.SpawnPos) < FTP.Config.SpawnProtectionRadius then
+            return true
+        end
+    end
+end)
+
+hook.Add("PlayerDeath", "FTP_Death", function(victim, inflictor, attacker)
+    if FTP:GetRole(victim) == "Innocent" then
+        timer.Simple(3, function()
+            if IsValid(victim) then victim:Spawn() end
+        end)
+    end
+
+    if FTP:GetRole(victim) == "Traitor" then
+        PrintMessage(HUD_PRINTTALK, "Traitor wurde getötet!")
+
+        if IsValid(attacker) and attacker:IsPlayer() then
+            FTP:AddMoney(attacker, FTP.Config.TraitorKillReward)
+        end
+
+        FTP.GameStarted = false
+    end
+end)
