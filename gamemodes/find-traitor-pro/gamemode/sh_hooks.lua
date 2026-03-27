8b6a7185051311dbddfe2c999f16c4fd157bd1a2
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-27 13:42:42 +0100

    Update sh_hooks.lua

--- gamemodes/find-traitor-pro/gamemode/sh_hooks.lua
+++ gamemodes/find-traitor-pro/gamemode/sh_hooks.lua
@@ -1,1 +1,12 @@
-
+hook.Add("PlayerSay", "FTP_Shop", function(ply, text)
+    if text == "!shop" then
+        if ply:GetNWInt("Money") >= 100 then
+            ply:Give("weapon_shotgun")
+            ply:SetNWInt("Money", ply:GetNWInt("Money") - 100)
+            ply:ChatPrint("Shotgun gekauft!")
+        else
+            ply:ChatPrint("Nicht genug Geld!")
+        end
+        return ""
+    end
+end)
