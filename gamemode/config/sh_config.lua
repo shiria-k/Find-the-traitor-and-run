c555a8f33182aa80c3f08e75c150393ce8bab693
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-27 13:26:12 +0100

    Delete gamemode directory

--- gamemode/config/sh_config.lua
+++
@@ -1,9 +0,0 @@
-FTTR = FTTR or {}
-
-FTTR.Config = {
-    MinPlayers = 4,
-    SpawnProtectionRadius = 1000, 
-    StartMoney = 0,
-    TraitorRewards = 50,
-    WeaponRespawnTime = 180,
-}
--- gamemode/shared.lua
+++
@@ -1,6 +0,0 @@
-GM.Name = "Find Traitor Pro"
-GM.Author = "You"
-GM.Email = ""
-GM.Website = ""
-
-DeriveGamemode("sandbox")
