0f4cbef84fba60e2e52532fb80042aeacd03d4eb
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-27 09:31:00 +0100

    Update sh_roles.lua

--- gamemode/core/sh_roles.lua
+++ gamemode/core/sh_roles.lua
@@ -1,1 +1,11 @@
+FTTR.Roles = {
+    INNOCENT = 1,
+    TRAITOR = 2
+}
 
+function FTTR.GetTraitorCount(plyCount)
+    if plyCount >= 20 then return 3 end
+    if plyCount >= 10 then return 2 end
+    if plyCount >= 4 then return 1 end
+    return 0
+end
