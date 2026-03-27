b61cb567d909de5df361821a51f554a84e12066c
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-27 13:43:19 +0100

    Update cl_init.lua

--- gamemodes/find-traitor-pro/gamemode/cl_init.lua
+++ gamemodes/find-traitor-pro/gamemode/cl_init.lua
@@ -1,1 +1,11 @@
+hook.Add("PostPlayerDraw", "FTP_Wins", function(ply)
+    local pos = ply:GetPos() + Vector(0,0,85)
+    local ang = LocalPlayer():EyeAngles()
 
+    ang:RotateAroundAxis(ang:Right(), 90)
+
+    cam.Start3D2D(pos, Angle(0, ang.y, 90), 0.2)
+        draw.SimpleText("Wins: "..ply:GetNWInt("Wins"),
+            "DermaDefault", 0, 0, Color(255,255,255), TEXT_ALIGN_CENTER)
+    cam.End3D2D()
+end)
