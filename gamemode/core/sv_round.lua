1de7a6ad55dc0f59e5fc062c11cdb3ef821ba047
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-27 09:54:38 +0100

    Update sv_round.lua

--- gamemode/core/sv_round.lua
+++ gamemode/core/sv_round.lua
@@ -36,3 +36,22 @@ function FTTR.AssignRoles()
         table.RemoveByValue(players, ply)
     end
 end
+hook.Add("PostDrawOpaqueRenderables", "FTTR_DrawWins", 0, function()
+    for _, ply in ipairs(player.GetAll()) do
+        if not IsValid(ply) then continue end
+
+        local pos = ply:GetPos() + Vector(0,0,80)
+        local ang = Angle(0, LocalPlayer():EyeAngles().y - 90, 90)
+
+        cam.Start3D2D(pos, ang, 0.25)
+            draw.SimpleText(
+                "Wins: " .. (ply.FTTR_Wins or 0),
+                "DermaLarge",
+                0, 0,
+                Color(255,255,0),
+                TEXT_ALIGN_CENTER,
+                TEXT_ALIGN_CENTER
+            )
+        cam.End3D2D()
+    end
+end)
