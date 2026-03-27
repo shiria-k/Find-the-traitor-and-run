ad27836856b6187da9ea72c711c840076fd4af7c
shiria-k <270121865+shiria-k@users.noreply.github.com>
2026-03-27 09:36:56 +0100

    Update sv_npc.lua

--- gamemode/system/sv_npc.lua
+++ gamemode/system/sv_npc.lua
@@ -1,1 +1,22 @@
+function FTTR.SpawnNPCs()
+    for i = 1, 15 do
+        local npc = ents.Create("npc_citizen")
+        if not IsValid(npc) then continue end
 
+        npc:SetPos(VectorRand() * 2000)
+        npc:Spawn()
+        npc:SetModel("models/player/Group01/tm_leet_variantA.mdl")
+
+        npc:SetKeyValue("targetname", "FTTR_NPC")
+
+        npc:AddRelationship("player D_HT 99")
+
+        function npc:Think()
+            for _, ply in ipairs(player.GetAll()) do
+                if ply.FTTR_Role == FTTR.Roles.INNOCENT then
+                    self:SetEnemy(ply)
+                end
+            end
+        end
+    end
+end
